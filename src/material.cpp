0296b8a6daa106bb5a325d13707dd230bf5e35c9
Robik81 <robert.polz@seznam.cz>
2020-10-15 17:26:20 +0200

    material breathability

--- src/material.cpp
+++ src/material.cpp
@@ -70,6 +70,7 @@ void material_type::load( const JsonObject &jsobj, const std::string & )
     optional( jsobj, was_loaded, "specific_heat_solid", _specific_heat_solid );
     optional( jsobj, was_loaded, "latent_heat", _latent_heat );
     optional( jsobj, was_loaded, "freeze_point", _freeze_point );
+    optional( jsobj, was_loaded, "breathability", _breathability );
 
     assign( jsobj, "salvaged_into", _salvaged_into );
     optional( jsobj, was_loaded, "repaired_with", _repaired_with, itype_id::NULL_ID() );
@@ -233,6 +234,11 @@ int material_type::freeze_point() const
 int material_type::density() const
 {
     return _density;
+}
+
+int material_type::breathability() const
+{
+    return _breathability;
 }
 
 bool material_type::edible() const
